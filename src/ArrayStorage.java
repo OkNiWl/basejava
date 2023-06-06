3711061e30d0b826aa9b11eb064957dbfd9ecede
Nikolay Okolelov <okniwl@gmail.com>
2023-06-06 21:48:21 +0700

    Solution of homework HW1 (red.#1)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,87 +1,52 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for (int i = 0; i <= storage.length - 1; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
+        for (int i = 0; i <= size - 1; i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i <= storage.length - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-
         if (uuid.matches(".*\\d+")) {
-            for (int i = 0; i <= storage.length - 1; i++) {
+            for (int i = 0; i <= size - 1; i++) {
                 if (storage[i].uuid.equals(uuid)) {
                     return storage[i];
                 }
             }
         }
-
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i <= storage.length - 1; i++) {
+        for (int i = 0; i <= size - 1; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                break;
+                storage[i] = storage[size - 1];
             }
         }
-
-        for (int i = 0; i <= storage.length - 2; i++) {
-            if (storage[i] == null && storage[i + 1] != null) {
-                storage[i] = storage[i + 1];
-                storage[i + 1] = null;
-            }
-        }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int countResume = 0;
-
-        for (int i = 0; i <= storage.length - 1; i++) {
-            if (storage[i] != null) {
-                countResume++;
-            }
-        }
-
-        Resume[] onlyResumes = new Resume[countResume];
-
-        for (int i = 0; i <= storage.length - 1; i++) {
-            if (storage[i] != null) {
-                onlyResumes[i] = storage[i];
-            }
-        }
-
-        return onlyResumes;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int size = 0;
-
-        for (int i = 0; i <= storage.length - 1; i++) {
-            if (storage[i] != null) {
-                size++;
-            }
-        }
-
         return size;
     }
 }
